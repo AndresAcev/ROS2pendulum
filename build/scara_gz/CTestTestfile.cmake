452bc63559003adf4420db4f747ab00afd74185f
Andres Acevedo <andres.acevedo98@hotmail.com>
2025-05-04 17:47:45 -0500

    Remove build directory

--- build/scara_gz/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ubuntu/cpm/scaraModified/ros-jazzy-scara/scara_gz
-# Build directory: /home/ubuntu/cpm/scaraModified/ros-jazzy-scara/build/scara_gz
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
